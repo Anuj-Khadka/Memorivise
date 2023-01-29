97420fdaf212ac01d09a5104727c97ccb2a9b812
Anuj-Khadka <anujkhadka008@gmail.com>
2023-01-29 18:43:57 +0545

    update home page

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,30 @@
-let descriptionBox = document.getElementById("description");
-let submitBtn = document.getElementById("submit");
-descriptionBox.addEventListener("keypress", (e) => {
-  if (e.key == "Enter") {
-    e.preventDefault();
-    submitBtn.click();
-  }
-  if (e.key == "Enter" && e.shiftKey) {
-    e.preventDefault();
-    console.log("hihihi");
-  }
-});
+// let descriptionBox = document.getElementById("description");
+// let submitBtn = document.getElementById("submit");
+// descriptionBox.addEventListener("keypress", (e) => {
+//   if (e.key == "Enter") {
+//     e.preventDefault();
+//     submitBtn.click();
+//   }
+//   if (e.key == "Enter" && e.shiftKey) {
+//     e.preventDefault();
+//     console.log("hihihi");
+//   }
+// });
+
+
+// search icon in navbar
+let searchIcon = document.getElementById("search-icon")
+let searchExpand = document.getElementById("search-expand")
+let searchSubmit = document.getElementById("search-submit")
+
+searchIcon.addEventListener("click", ()=>{
+  searchIcon.classList.add("d-none")
+  searchExpand.classList.remove("d-none")
+})
+
+
+searchSubmit.addEventListener("click", ()=>{
+  searchIcon.classList.toggle("d-none")
+  searchExpand.classList.toggle("d-none")
+
+})
