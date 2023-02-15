f1c30cd16f77420a571c37a04bc33f3b0830d9e8
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-15 20:59:33 +0545

    update models

---
+++ memorivise/models.py
@@ -0,0 +1,22 @@
+from flask_login import UserMixin
+from datetime import datetime
+from . import db
+
+
+class Revise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f'{self.id} - {self.text}'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    username = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    revise = db.relationship('Revise')
