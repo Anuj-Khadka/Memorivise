33e6ed32777db732d2d78d39da50827f5f8b5b87
Anuj-Khadka <anujkhadka008@gmail.com>
2023-02-18 23:22:46 +0545

    update signin

--- memorivise/auth.py
+++ memorivise/auth.py
@@ -1,25 +1,48 @@
-from flask import Blueprint, render_template, request, flash
-
+from flask import Blueprint, Flask, render_template, request, flash, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
+    # if request.method == 'POST':
+    #     email = request.form['email']
+    #     password = request.form['password']
+
+    #     user = User.query.filter_by(email=email).first()
+    #     if user:
+    #         if check_password_hash(user.password, password):
+    #             return redirect('/')
+    #         else:
+    #             flash("incorect password", category="error")
+    #     else:
+    #         flash("")
     return render_template('login.html')
 
 
 @auth.route("/signup", methods=['POST', 'GET'])
 def signup():
-    # if request.method == 'POST':
-    #     email = request.form['email']
-    #     username = request.form['username']
-    #     password = request.form['password']
-    #     confirmPassword = request.form['confirm-password']
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        confirmPassword = request.form['confirm-password']
 
-    #     if len(password) < 8:
-    #         flash("password is short", category="error")
+        # if len(password) < 8:
+        #     flash("password is short", category="error")
 
-    return render_template('signup.html')
+        new_user = User(email=email, username=username, password=generate_password_hash(password, method="sha256"))
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            # flash('Account created!', category='success')
+            return redirect('/revise')
+        except:
+            return "there was a problem adding the user"
+    else:
+        return render_template('signup.html')
 
 
 @auth.route("/logout")
