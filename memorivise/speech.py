e4f970ae1b7516329ec3a993c9490bef7f365d60
Anuj-Khadka <anujkhadka008@gmail.com>
2023-07-31 01:03:39 +0545

    update speech recorder

--- memorivise/speech.py
+++ memorivise/speech.py
@@ -5,31 +5,33 @@ def transcribe():
     # Create a Recognizer object
     r = sr.Recognizer()
 
-    # Specify the audio file path
-    audio_file = "/home/algae/Public/Memorivise/Neovim in 100 Seconds.wav"
+        # Specify the audio file path
+    #  audio_file = "/home/algae/Public/Memorivise/Neovim in 100 Seconds.wav"
 
     # Load the audio file into a AudioFile object
     # with sr.AudioFile(audio_file) as source:
     # audio = r.record(source)  # Read the entire audio file and store it in a variable
 
+
     # Use the microphone as source
     with sr.Microphone() as source:
         # Adjust for ambient noise
         r.adjust_for_ambient_noise(source)
-        flash("Speak something...", category="success")
+        # flash("Speak something...", category="success")
         print("Speak something...")
         # Listen for audio and store it in a variable
-        audio = r.listen(source, timeout=5.0)
+        audio = r.listen(source, timeout=10.0)
 
+    # Speech recognition
     try:
-        # Use Google Speech Recognition to transcribe the audio
         text = r.recognize_google(audio)
-
         print(f"You said: {text}")
         return text
     except sr.UnknownValueError:
         cantHear = "Oops! Could not understand audio."
+        # flash(cantHear, category="error")
         return cantHear
     except sr.RequestError as e:
         err = f"Error: {e}"
         return err
+
